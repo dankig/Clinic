235214844e7db451c57d29142512f60d5a973a89
dankig <dankig@outlook.com>
2018-09-04 02:33:41 +0300

    Пробная заливка

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		navigation: true,
+		slidesNavigation: true,
+		//кнопки навигации по слайдам
+		controlArrows: false,
+	});
+
+	
+});
