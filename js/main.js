8774302cf94df454339fd6e9bfb5334a4613404f
dankig <dankig@outlook.com>
2018-09-04 19:07:10 +0300

    Изменения в меню

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@ $(document).ready(function() {
 		//кнопки навигации по слайдам
 		controlArrows: false,
 	});
+});
 
-	
+$(function(){
+    var flag = 0;
+    $('.menu-icon').on('click', function(e){
+      e.preventDefault();
+      if(flag == 0 ){
+        $('.menu-icon').addClass("active");
+        $('.imenu').addClass('visible'); 
+        flag = 1;
+      }else {
+        $('.menu-icon').removeClass("active");
+        $('.imenu').removeClass('visible');
+        flag = 0;
+      }
+   });
 });
