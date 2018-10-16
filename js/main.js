c5ac28bae29ceeae312430dc8e00d5fc199c6f20
dankig <dankig@outlook.com>
2018-10-16 10:41:01 +0500

    Изменения в меню

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
+    //options here
+    anchors:['section0','section1','section2','section3'],
+    //menu:'#menu',
+		autoScrolling: true,
 		scrollHorizontally: true,
 		navigation: true,
-		slidesNavigation: true,
+    slidesNavigation: true,
+    //loopHorizontal: true,
 		//кнопки навигации по слайдам
-		controlArrows: false,
+    controlArrows: false,
+    //scrollOverflow: true,
 	});
 });
 
+var flag = 0;
+var active = 'imenu';
 $(function(){
-    var flag = 0;
-    $('.menu-icon').on('click', function(e){
+    
+    $('#c-menu').on('click', function(e){
       e.preventDefault();
       if(flag == 0 ){
         $('.menu-icon').addClass("active");
@@ -21,12 +27,26 @@ $(function(){
                           "position": "absolute",
                           "left" : "0px"  
         });
+        $('#hov').text('ЗАКРЫТЬ МЕНЮ');
         flag = 1;
-      }else {
+      }
+      else {
         $('.menu-icon').removeClass("active");
         $('.imenu').removeClass('visible');
         $('.imenu').css({"display": "none"});
+        $('#hov').text('МЕНЮ');
         flag = 0;
       }
-   });
+    });
 });
+
+var active = 'imenu';
+        $(document).ready(function(){
+            $('.ili').click(function(e){
+              $('.menu-icon').removeClass("active");
+              $('.imenu').removeClass('visible');
+              $('.imenu').css({"display": "none"});
+              $('#hov').text('МЕНЮ');
+              flag = 0;   
+            })
+        })
